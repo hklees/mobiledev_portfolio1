88b902fb685bea4a102b8f79cc1ad6f0fde7961b
hklees <hklees@knights.ucf.edu>
2022-03-22 15:04:30 -0400

    March 22, 2022 3:04 PM

--- src/components/extra.js
+++ src/components/extra.js
@@ -2,22 +2,30 @@ import React, {useState} from "react";
 
 
 function ButtonMessage() {
-    const [cart, setCart] =
-    useState(true);
+    const [count, setCount] =
+    useState(0);
   
     return (
-      <div className="buttonMessage">
-        { 
-          ( cart
-          ?
-            <>
-        <button onClick={()=> {setCart(false)}}>Add to Cart </button>
-        </>
-        :
-          alert('Item added to cart')
-        )
-      }
-      </div>
+//       <div className="buttonMessage">
+//         { 
+//           ( cart
+//           ?
+//             <>
+//         <button onClick={()=> {setCart(false)}}>Add to Cart </button>
+//         </>
+//         :
+//           alert('Item added to cart')
+//         )
+//       }
+//       </div>
+//     );
+//   }
+    <div>
+        <p> Item has been added to cart {count} times.</p>
+        <button> onClick={() => setCount (count + 1)}
+        Add to Cart</button>
+    </div>
     );
-  }
+}
+
   export default ButtonMessage;
